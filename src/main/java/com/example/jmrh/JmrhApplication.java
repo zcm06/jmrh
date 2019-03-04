58509f191a064c2b30138df806096a36f0d4e59f
张灿明 <635677619@qq.com>
2019-03-04 21:27:45 +0800

    Initial commit

---
+++ src/main/java/com/example/jmrh/JmrhApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmrhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmrhApplication.class, args);
+    }
+
+}
