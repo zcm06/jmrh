df83732a05889e56d39eec36791007dab9b86b61
张灿明 <635677619@qq.com>
2019-03-19 21:59:16 +0800

    添加方法

--- src/main/java/com/example/jmrh/config/WebConfigurer.java
+++ src/main/java/com/example/jmrh/config/WebConfigurer.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author: ZHANG CANMING
  * @create: 2019-03-13 21:01
  **/
+@Configuration
 public class WebConfigurer implements WebMvcConfigurer {
 
     @Autowired
