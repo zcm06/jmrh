adce631132d34afcba4bbf82cde88d575fe1166c
zhangcanming <635677619@qq.com>
2019-03-27 08:52:28 +0800

    添加TableField

--- src/main/java/com/example/jmrh/utils/TableField.java
+++ src/main/java/com/example/jmrh/utils/TableField.java
@@ -6,7 +6,8 @@ public enum TableField {
     legalRepresentative("法定代表人"),unitCreateTime("单位创建时间"),tel("联系电话"),
     registeredCapital("注册资金"),fixedTotalAssets("固定总资产"),annualMainBusinessIncome("年度主营业务收入"),
     annualRdInvestment("年度研发投入"),totalPeople("总人数"),developersNumber("研发人数"),businessScope("经营范围"),
-    mainProducts("主要产品"),hightechEnterprises("高新技术企业"),listing("上市"),capitalComponent("资本成分"),
+    mainProducts("主要产品"),registeredAddress("注册地址"),researchProductionAddress("科研地址"),contactAddress("通信地址"),
+    hightechEnterprises("高新技术企业"),listing("上市"),capitalComponent("资本成分"),
     unitCategory("单位类别"),industryCategory("行业类别"),militarySalary("军工资质"),unitOverview("单位概况");
 
     private String value;
